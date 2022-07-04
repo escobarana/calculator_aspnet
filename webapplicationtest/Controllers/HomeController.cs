41b57fd6beb6c7f84ed90dc502bab1884394f6b9
Ana Escobar <anaescobar8@gmail.com>
2022-07-04 12:19:31 +0200

    Feat: Calculator with ASP.NET
    Enterprise Programming

---
+++ webapplicationtest/Controllers/HomeController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace webapplicationtest.Controllers
+{
+    public class HomeController : Controller
+    {
+        // [Route("/Home/Calculator/{?id}")]
+        public IActionResult Index() // (int? id)
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Index(int partOne, char operand, int partTwo)
+        {
+            switch (operand)
+            {
+                case '+':
+                    ViewData["result"] = partOne + partTwo;
+                    break;
+                case '-':
+                    ViewData["result"] = partOne - partTwo;
+                    break;
+                case '/':
+                    ViewData["result"] = (float) partOne / (float) partTwo;
+                    break;
+                case '*':
+                    ViewData["result"] = partOne * partTwo;
+                    break;
+                case '%':
+                    ViewData["result"] = partOne % partTwo;
+                    break;
+            }
+
+            ViewData["partOne"] = partOne;
+            ViewData["partTwo"] = partTwo;
+            ViewData["operand"] = operand;
+            return View();
+        }
+    }
+}
